c7af63ae1515fa5243d4df29c320b56336ce703d
Brenna Stuart <98294995+brennacodes@users.noreply.github.com>
2022-07-26 17:54:42 -0600

    US1: add merchant dashboard; show merchant name

--- app/models/merchant.rb
+++ app/models/merchant.rb
@@ -1,4 +1,8 @@
 class Merchant < ApplicationRecord
+  has_many :items
+  has_many :merchant_invoices
+  has_many :invoices, through: :merchant_invoices
+  has_many :customers, through: :invoices
 
   validates_presence_of :name
 end
