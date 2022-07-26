3afc4564a8e5650519b5ee091673c3cbd03898cc
Brenna Stuart <98294995+brennacodes@users.noreply.github.com>
2022-07-26 11:04:17 -0600

    Add csv_load notes

--- app/models/merchant_invoice.rb
+++ app/models/merchant_invoice.rb
@@ -1,4 +1,6 @@
 class MerchantInvoice < ApplicationRecord
   belongs_to :merchant
   belongs_to :invoice
+
+  validates_presence_of :merchant_id, :invoice_id, :created_at, :updated_at
 end
--- app/models/transaction.rb
+++ app/models/transaction.rb
@@ -1,3 +1,6 @@
 class Transaction < ApplicationRecord
   belongs_to :invoice
+  enum result: { success: 0, failed: 1 }
+
+  validates_presence_of :invoice_id, :credit_card_number, :credit_card_expiration_date, :result, :created_at, :updated_at
 end
