e951ba363bb2b216a5ccd2b1c187cb52874e4928
Brenna Stuart <98294995+brennacodes@users.noreply.github.com>
2022-07-27 14:14:46 -0600

    US3: invoice relations working

--- app/models/invoice.rb
+++ app/models/invoice.rb
@@ -1,10 +1,10 @@
 class Invoice < ApplicationRecord
   belongs_to :customer
-  
+
   has_many :invoice_items
   has_many :transactions
   has_many :items, through: :invoice_items
-  has_many :merchants, through: :invoice_items, source: :items
+  has_many :merchants, through: :invoice_items, source: :item
 
   enum status: { cancelled: 0, "in progress" => 1, completed: 2 }
 
