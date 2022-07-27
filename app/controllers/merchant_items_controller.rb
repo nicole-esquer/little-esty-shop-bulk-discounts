574f7ff5326c70b8d8669f57979f43a032ad6b53
Nikky Rojas <rojas7313@gmail.com>
2022-07-27 11:15:44 -0600

    add possible prepopulated form

--- app/controllers/merchant_items_controller.rb
+++ app/controllers/merchant_items_controller.rb
@@ -11,12 +11,14 @@ class MerchantItemsController < ApplicationController
     def edit
     @merchant = Merchant.find(params[:merchant_id])
     @item = Item.find(params[:id])
+    @name = @item.name
     end
 
   def update
     item = Item.find(params[:id])
     if item.update(item_params)
       redirect_to "/merchants/#{@merchant.id}/items/#{@item.id}"
+      flash[:alert] = "Information has been successfully updated!"
     else
       redirect_to "/merchants/#{@merchant.id}/items/#{@item.id}/edit"
     #   flash[:alert] = "Error: #{error_message(item.errors)}"
