b988bc7b23dd50cc917a7ed61ecabd073730e7e5
Nicole Esquer <nicole.esquer5@gmail.com>
2022-07-26 23:34:54 -0700

    created tests for us 21 and 22

--- spec/features/admin/index_spec.rb
+++ spec/features/admin/index_spec.rb
@@ -5,7 +5,7 @@ RSpec.describe 'admin dashboard' do
       visit '/admin'
 
       expect(page).to have_content("Admin Dashboard")
-  end
+   end
 
    it 'has a link to the admin merchants index (/admin/merchants)' do
       visit '/admin'
@@ -20,5 +20,57 @@ RSpec.describe 'admin dashboard' do
 
       expect(current_path).to eq(admin_invoices_path)
    end
+
+   describe 'dashboard statistics - top customers' do
+      xit 'displays the names of the top 5 customers' do
+         visit '/admin'
   
+         expect(page).to have_content("Top 5 Customers")
+
+         sally = Customer.create!(first_name: "Sally", last_name: "Sunshine")
+         henry = Customer.create!(first_name: "Henry", last_name: "Hill")
+         tom = Customer.create!(first_name: "Tom", last_name: "Thunder")
+         mary = Customer.create!(first_name: "Mary", last_name: "Mountain")
+         riley = Customer.create!(first_name: "Riley", last_name: "Rain")
+   
+         invoice_1 = sally.invoices.create!(status: "In Progress")
+         invoice_2 = sally.invoices.create!(status: "Completed")
+         invoice_3 = sally.invoices.create!(status: "Cancelled")
+         invoice_4 = henry.invoices.create!(status: "Completed")
+         invoice_5 = tom.invoices.create!(status: "Completed")
+         invoice_6 = tom.invoices.create!(status: "Completed")
+         invoice_7 = mary.invoices.create!(status: "Completed")
+         invoice_8 = mary.invoices.create!(status: "Completed")
+         invoice_9 = mary.invoices.create!(status: "Completed")
+         invoice_10 = mary.invoices.create!(status: "Completed")
+
+         expect(page).to have_content("Sally - 1 Purchases")
+      end
+   end
+
+   describe 'Admin Dashboard Invoices' do
+      it 'has the "Incomplete Invoices" section w/ list of IDs for all invoices with unshipped items' do
+         sally = Customer.create!(first_name: "Sally", last_name: "Sunshine")
+         henry = Customer.create!(first_name: "Henry", last_name: "Hill")
+
+         invoice_1 = sally.invoices.create!(status: "In Progress")
+         invoice_2 = sally.invoices.create!(status: "In Progress")
+         invoice_3 = sally.invoices.create!(status: "In Progress")
+         invoice_4 = henry.invoices.create!(status: "Completed")
+
+         visit '/admin'
+
+         expect(page).to have_content("Incomplete Invoices")
+   
+         expect(page).to have_content(@invoice_1.id)
+         expect(page).to have_content(@invoice_2.id)
+         expect(page).to have_content(@invoice_3.id)
+         
+         expect(page).to_not have_content(@invoice_3.id)
+      end
+
+      xit 'each invoice id links to that invoices admin show page' do
+   
+      end
+   end
 end
