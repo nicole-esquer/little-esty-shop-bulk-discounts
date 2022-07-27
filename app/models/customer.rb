51e95c472ebc661ed372f72cb87702cbacb214e6
Brenna Stuart <98294995+brennacodes@users.noreply.github.com>
2022-07-27 14:22:50 -0600

    US3: customer model relations working

--- app/models/customer.rb
+++ app/models/customer.rb
@@ -3,7 +3,7 @@ class Customer < ApplicationRecord
   has_many :invoice_items, through: :invoices
   has_many :transactions, through: :invoices
   has_many :items, through: :invoices, source: :invoice_items
-  has_many :merchants, through: :items, source: :invoice_items
+  has_many :merchants, through: :invoices
 
   validates_presence_of :first_name, :last_name
 
