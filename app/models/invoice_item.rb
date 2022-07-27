828b3c8de13097574ca888a6be8ab84e23056f8d
Brenna Stuart <98294995+brennacodes@users.noreply.github.com>
2022-07-27 14:18:59 -0600

    US3: invoice_item model relations working

--- app/models/invoice_item.rb
+++ app/models/invoice_item.rb
@@ -2,7 +2,7 @@ class InvoiceItem < ApplicationRecord
   belongs_to :item
   belongs_to :invoice
 
-  has_many :transactions, through: :invoice
+  has_one :transactions, through: :invoice
   
   enum status: { packaged: 0, pending: 1, shipped: 2 }
 
