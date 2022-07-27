9944ede2512c3ad94ef78f54a3d52af54a612daa
Nicole Esquer <nicole.esquer5@gmail.com>
2022-07-26 23:23:32 -0700

    method models for top customer-WIP

--- app/models/customer.rb
+++ app/models/customer.rb
@@ -1,3 +1,9 @@
 class Customer < ApplicationRecord
   validates_presence_of :first_name, :last_name
+
+  has_many :invoices, dependent: :destory
+
+  def top_5_customers
+    require 'pry'; binding.pry
+  end
 end
